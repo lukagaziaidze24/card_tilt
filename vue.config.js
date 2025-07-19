bf9642f88fb8875ecde0ef49c81de066c9657db4
lukagaziaidze24 <lukagaziaidze@gmail.com>
2025-07-19 11:52:27 +0400

    commented https configs in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem'),
-    },
-    allowedHosts: 'all',
-  },
+  // devServer: {
+  //   host: '0.0.0.0',
+  //   port: 8080,
+  //   https: {
+  //     key: fs.readFileSync('./certs/key.pem'),
+  //     cert: fs.readFileSync('./certs/cert.pem'),
+  //   },
+  //   allowedHosts: 'all',
+  // },
   css: {
     loaderOptions: {
       scss: {
